72ebbc6627964f27f78206f459ca421f4e0df1d9
Prabhat <mysql.prabhat@gmail.com>
2025-12-02 03:12:06 +0530

    fix

--- app/auth/google/callback.tsx
+++ app/auth/google/callback.tsx
@@ -4,14 +4,12 @@ export default function Callback() {
 
   useEffect(() => {
     const url = new URL(window.location.href);
-    const token = url.searchParams.get("accessToken");
-    console.log("Received:", token);
+    const token = url.searchParams.get("accessToken") ?? "";
+    // save token
+    localStorage.setItem("token", token);
 
-        // save token
-        localStorage.setItem("token", token);
-
-        // redirect to dashboard
-        window.location.href = "/products";
+    // redirect to dashboard
+    window.location.href = "/products";
     
   }, []);
 
