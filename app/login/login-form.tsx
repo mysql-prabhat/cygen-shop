60bef39f33e97cf153da919a2416776d92606714
Prabhat <mysql.prabhat@gmail.com>
2025-12-04 01:43:00 +0530

    google login path changes

--- app/login/login-form.tsx
+++ app/login/login-form.tsx
@@ -1,6 +1,6 @@
 "use client";
 function googleLogin() {
-  window.location.href = "http://localhost:4000/api/auth/google";
+  window.location.href = "https://cygen-shop-backend-production.up.railway.app/api/auth/google";
 }
 
 export default function Login() {
